cb772be66d67a6d8553ff35828e5d50e9bc02cdf
charlieroth <charlieroth4@icloud.com>
2023-02-26 18:21:47 +0100

    pacman-rules: complete

--- elixir/pacman-rules/lib/rules.ex
+++ elixir/pacman-rules/lib/rules.ex
@@ -1,17 +1,25 @@
 defmodule Rules do
   def eat_ghost?(power_pellet_active, touching_ghost) do
     # Please implement the eat_ghost?/2 function
+    power_pellet_active and touching_ghost
   end
 
   def score?(touching_power_pellet, touching_dot) do
     # Please implement the score?/2 function
+    touching_power_pellet or touching_dot
   end
 
   def lose?(power_pellet_active, touching_ghost) do
     # Please implement the lose?/2 function
+    not power_pellet_active and touching_ghost
   end
 
   def win?(has_eaten_all_dots, power_pellet_active, touching_ghost) do
     # Please implement the win?/3 function
+    cond do
+      has_eaten_all_dots and not touching_ghost -> true
+      has_eaten_all_dots and power_pellet_active and touching_ghost -> true
+      true -> false
+    end
   end
 end
