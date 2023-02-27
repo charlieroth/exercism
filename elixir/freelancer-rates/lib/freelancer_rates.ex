2d70ed9bd54d3aaa393fa706d88c0a0317d32432
charlieroth <charlieroth4@icloud.com>
2023-02-27 15:25:30 +0100

    freelancer_rates: complete

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,20 +1,41 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
     8.0 * hourly_rate
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
-    (before_discount * discount) / 1.0
+    discount
+      |> (&((100.0 - &1) / 100.0)).()
+      |> (&(&1 * before_discount)).()
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
-    22.00 * daily_rate(hourly_rate) * discount
+    if discount > 0.0 do
+      hourly_rate
+        |> daily_rate()
+        |> apply_discount(discount)
+        |> (&(&1 * 22.0)).()
+        |> ceil()
+    else
+      hourly_rate
+        |> daily_rate()
+        |> (&(&1 * 22.0)).()
+        |> ceil()
+    end
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    if discount > 0.0 do
+      hourly_rate
+        |> daily_rate()
+        |> apply_discount(discount)
+        |> (&(budget / &1)).()
+        |> Float.floor(1)
+    else
+      hourly_rate
+        |> daily_rate()
+        |> (&(budget / &1)).()
+        |> Float.floor(1)
+    end
   end
 end
