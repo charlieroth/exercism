1773345623460078510452a70d2f593764833d69
charlieroth <charlieroth4@icloud.com>
2023-02-28 16:44:50 +0100

    guessing-game: complete

--- elixir/guessing-game/lib/guessing_game.ex
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -1,5 +1,11 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
-    # Please implement the compare/2 function
+  def compare(secret_number, guess \\ nil) do
+    respond(secret_number, guess)
   end
+
+  def respond(_secret, guess) when not is_integer(guess), do: "Make a guess"
+  def respond(secret, guess) when abs(secret - guess) == 1, do: "So close"
+  def respond(secret, guess) when guess < secret, do: "Too low"
+  def respond(secret, guess) when guess > secret, do: "Too high"
+  def respond(_secret, _guess), do: "Correct"
 end
