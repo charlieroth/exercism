2517ea4f8aea6f706ec83b62c4b6611b0219f4a1
charlieroth <charlieroth4@icloud.com>
2023-03-02 15:55:17 +0100

    leap: complete

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,3 +1,19 @@
-export function isLeap() {
-  throw new Error('Remove this statement and implement this function')
+function divisibleBy(num: number, divisor: number): boolean {
+  return num % divisor === 0;
+}
+
+export function isLeap(year: number): boolean {
+  if (divisibleBy(year, 4)) {
+    if (divisibleBy(year, 100) && !divisibleBy(year, 400)) {
+      return false;
+    }
+
+    if (!divisibleBy(year, 100) && divisibleBy(year, 400)) {
+      return true;
+    }
+
+    return true;
+  }
+
+  return false;
 }
