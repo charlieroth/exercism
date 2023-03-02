6d969cf18318c5711aab751cb341250693062eb6
charlieroth <charlieroth4@icloud.com>
2023-03-02 16:41:56 +0100

    matrix: complete

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,13 +1,30 @@
 export class Matrix {
-  constructor() {
-    throw new Error('Remove this statement and implement this function')
+  private _rows: number[][];
+
+  constructor(input: string) {
+    const rows: number[][] = [];
+    for (const line of input.split("\n")) {
+      const row = line.split(" ").map((n) => parseInt(n));
+      rows.push(row);
+    }
+    this._rows = rows;
   }
 
-  get rows(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get rows(): number[][] {
+    return this._rows;
   }
 
-  get columns(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get columns(): number[][] {
+    const columns: number[][] = [];
+
+    for (let i = 0; i < this._rows.length; i++) {
+      const column: number[] = [];
+      for (let j = 0; j < this._rows.length; j++) {
+        column.push(this._rows[j][i]);
+      }
+      columns.push(column);
+    }
+
+    return columns;
   }
 }
