9a53aa04f234e313cffc195ace4991082b04ba30
charlieroth <charlieroth4@icloud.com>
2023-03-02 16:02:49 +0100

    rna-transcription: complete

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,3 +1,28 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+enum Strands {
+  G = "G",
+  C = "C",
+  T = "T",
+  A = "A",
+  U = "U",
+}
+
+type Strand = `${Strands}`;
+
+function transcribe(strand: string): Strand {
+  switch (strand) {
+    case Strands.G:
+      return Strands.C;
+    case Strands.C:
+      return Strands.G;
+    case Strands.T:
+      return Strands.A;
+    case Strands.A:
+      return Strands.U;
+    default:
+      throw new Error("Invalid input DNA.");
+  }
+}
+
+export function toRna(strands: string) {
+  return strands.split("").map(transcribe).join("");
 }
