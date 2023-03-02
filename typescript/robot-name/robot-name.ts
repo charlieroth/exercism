9a6dc2139e6c34c7663543cce32c6c1692c0828c
charlieroth <charlieroth4@icloud.com>
2023-03-02 17:06:45 +0100

    robot-name: wip

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,15 +1,56 @@
+function allPossibleNames() {
+  const names: string[] = [];
+  for (let i = 0; i < 25; i++) {
+    for (let j = 0; j < 25; j++) {
+      for (let k = 0; k < 10; k++) {
+        for (let l = 0; l < 10; l++) {
+          for (let m = 0; m < 10; m++) {
+            const _i = String.fromCharCode(97 + i).toUpperCase();
+            const _j = String.fromCharCode(97 + j).toUpperCase();
+            names.push(`${_i}${_j}${k}${l}${m}`);
+          }
+        }
+      }
+    }
+  }
+
+  return names;
+}
+
+const possibleNames = allPossibleNames();
+
 export class Robot {
-  constructor() {}
+  private _name: string;
+  private previousNameIndicies: Set<number>;
+  private numberOfNames = 25 * 25 * 10 * 10 * 10;
+
+  constructor() {
+    const nameIndex = Math.floor(Math.random() * this.numberOfNames);
+
+    this.previousNameIndicies = new Set<number>();
+    this.previousNameIndicies.add(nameIndex);
+
+    this._name = possibleNames[nameIndex];
+  }
 
   public get name(): string {
-    throw new Error('Implement Robot#name')
+    return this._name;
   }
 
   public resetName(): void {
-    throw new Error('Implement Robot#resetName')
+    let curr = Math.floor(Math.random() * this.numberOfNames);
+    let cont = true;
+    while (cont) {
+      if (!this.previousNameIndicies.has(curr)) {
+        this.previousNameIndicies = new Set<number>();
+        this.previousNameIndicies.add(curr);
+        this._name = possibleNames[curr];
+        cont = false;
+      }
+    }
   }
 
   public static releaseNames(): void {
-    throw new Error('Implement Robot.releaseNames')
+    this.possibleNames = [];
   }
 }
