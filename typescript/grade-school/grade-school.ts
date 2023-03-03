3542bd64862adf2208ae64082ee43ab5c0a5cb08
charlieroth <charlieroth4@icloud.com>
2023-03-03 08:54:46 +0100

    grade-school: wip

--- typescript/grade-school/grade-school.ts
+++ typescript/grade-school/grade-school.ts
@@ -1,13 +1,43 @@
 export class GradeSchool {
+  private db: Record<number, readonly string[]>;
+
+  constructor() {
+    this.db = {};
+  }
+
   roster() {
-    throw new Error('Remove this statement and implement this function')
+    return Object.freeze(this.db);
   }
 
-  add() {
-    throw new Error('Remove this statement and implement this function')
+  add(student: string, grade: number) {
+    const isStudentInAnotherGrade = false;
+    for (const [k, v] of Object.entries(this.db)) {
+      for (const s of v) {
+        // @ts-ingore
+        if (grade === k && s === student) {
+        }
+      }
+    }
+
+    if (isStudentInAnotherGrade) {
+      this.db[grade] = Object.freeze(
+        this.db[grade].filter((s) => s !== student)
+      );
+      return;
+    }
+
+    if (this.db[grade]) {
+      this.db[grade] = this.db[grade].concat([student]).sort();
+    } else {
+      this.db[grade] = [student];
+    }
   }
 
-  grade() {
-    throw new Error('Remove this statement and implement this function')
+  grade(grade: number) {
+    if (!this.db[grade]) {
+      return Object.freeze([]);
+    }
+
+    return Object.freeze(this.db[grade]);
   }
 }
