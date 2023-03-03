7faabe5880d102bb5abea2d1f9a8f36c26469dd0
charlieroth <charlieroth4@icloud.com>
2023-03-03 10:31:53 +0100

    freelancer-rates: complete

--- javascript/freelancer-rates/freelancer-rates.js
+++ javascript/freelancer-rates/freelancer-rates.js
@@ -1,23 +1,4 @@
 // @ts-check
-//
-// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion on the web
-// and supported IDEs when implementing this exercise. You don't need to
-// understand types, JSDoc, or TypeScript in order to complete this JavaScript
-// exercise, and can completely ignore this comment block and directive.
-
-// 👋🏽 Hi again!
-//
-// A quick reminder about exercise stubs:
-//
-// 💡 You're allowed to completely clear any stub before you get started. Often
-// we recommend using the stub, because they are already set-up correctly to
-// work with the tests, which you can find in ./freelancer-rates.spec.js.
-//
-// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
-// in idiomatic JavaScript, but some companies and style-guides do enforce them.
-//
-// Get those rates calculated!
 
 /**
  * The day rate, given a rate per hour
@@ -26,7 +7,7 @@
  * @returns {number} the rate per day
  */
 export function dayRate(ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  return ratePerHour * 8;
 }
 
 /**
@@ -37,7 +18,8 @@ export function dayRate(ratePerHour) {
  * @returns {number} the number of days
  */
 export function daysInBudget(budget, ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  const numDays = budget / dayRate(ratePerHour);
+  return Math.floor(numDays);
 }
 
 /**
@@ -49,5 +31,13 @@ export function daysInBudget(budget, ratePerHour) {
  * @returns {number} the rounded up discounted rate
  */
 export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
-  throw new Error('Remove this line and implement the function');
+  const months = Math.floor(numDays / 22);
+  const daysLeft = numDays % 22;
+
+  const dailyRate = daysLeft * dayRate(ratePerHour);
+
+  const monthlyRate = 22 * dayRate(ratePerHour) * months;
+  const discountedMonthlyRate = Math.ceil(monthlyRate * (1 - discount));
+
+  return Math.ceil(discountedMonthlyRate + dailyRate);
 }
