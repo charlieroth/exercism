19c08c3cc3e1df506f45980a4516fb07f9df52d6
charlieroth <charlieroth4@icloud.com>
2023-03-08 16:54:09 +0100

    name-badge: complete

--- elixir/name-badge/lib/name_badge.ex
+++ elixir/name-badge/lib/name_badge.ex
@@ -1,5 +1,24 @@
 defmodule NameBadge do
+  @doc """
+  The point of this exercise is to use if statements
+
+  This could easily be done with pattern matching in the function
+  definition
+  """
+  @spec print(String.t() | nil, String.t() | nil, String.t() | nil) :: String.t()
   def print(id, name, department) do
-    # Please implement the print/3 function
+    if is_nil(id) do
+      if is_nil(department) do
+        "#{name} - OWNER"
+      else
+        "#{name} - #{String.upcase(department)}"
+      end
+    else
+      if is_nil(department) do
+        "[#{id}] - #{name} - OWNER"
+      else
+        "[#{id}] - #{name} - #{String.upcase(department)}"
+      end
+    end
   end
 end
