392e205e43b0a3e9628b41ab877d061b207ced5f
charlieroth <charlieroth4@icloud.com>
2023-03-08 10:10:17 +0100

    leap: complete

--- elixir/leap/lib/year.ex
+++ elixir/leap/lib/year.ex
@@ -10,5 +10,13 @@ defmodule Year do
   """
   @spec leap_year?(non_neg_integer) :: boolean
   def leap_year?(year) do
+    div_by_four = rem(year, 4) == 0
+    div_by_one_hund = rem(year, 100) == 0
+    div_by_four_hund = rem(year, 400) == 0
+    is_leap_year?(div_by_four, div_by_one_hund, div_by_four_hund)
   end
+
+  defp is_leap_year?(true, _, true), do: true
+  defp is_leap_year?(true, false, _), do: true
+  defp is_leap_year?(_, _, _), do: false
 end
