118fe20322d48dfd54f7d1da372e0821e551aac9
charlieroth <charlieroth4@icloud.com>
2023-03-08 16:37:45 +0100

    german-sysadmin: complete (2)

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,28 +1,18 @@
 defmodule Username do
   def sanitize(''), do: ''
 
-  def sanitize(username) do
-    try do
-      username
-      |> to_string()
-      |> String.replace(~r/[\x{00DF}]/u, "ss")
-      |> String.replace(~r/[\x{00E4}]/u, "ae")
-      |> String.replace(~r/[\x{00F6}]/u, "oe")
-      |> String.replace(~r/[\x{00FC}]/u, "ue")
-      |> String.replace(~r/\d/u, "")
-      |> String.codepoints()
-      |> Enum.filter(&Regex.match?(~r/[\w_]/, &1))
-      |> Enum.join()
-      |> to_charlist()
-    rescue
-      _ -> ''
-    end
+  def sanitize([head | tail]) do
+    sanitized =
+      case head do
+        ?ä -> 'ae'
+        ?ö -> 'oe'
+        ?ü -> 'ue'
+        ?ß -> 'ss'
+        c when c >= ?a and c <= ?z -> [c]
+        ?_ -> '_'
+        _ -> ''
+      end
 
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
-
-    # Please implement the sanitize/1 function
+    sanitized ++ sanitize(tail)
   end
 end
