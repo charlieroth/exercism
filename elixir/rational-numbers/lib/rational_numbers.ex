d8cc87eade4102bc751351387c6f4b512ebd18e1
charlieroth <charlieroth4@icloud.com>
2023-03-08 12:12:24 +0100

    rational-numbers: wip

--- elixir/rational-numbers/lib/rational_numbers.ex
+++ elixir/rational-numbers/lib/rational_numbers.ex
@@ -6,55 +6,69 @@ defmodule RationalNumbers do
   """
   @spec add(a :: rational, b :: rational) :: rational
   def add({a1, a2} = _a, {b1, b2} = _b) do
-    {((a1 * b2) + (a2 * b1)), b1 * b2}
+    reduce({a1 * b2 + a2 * b1, b1 * b2})
   end
 
   @doc """
   Subtract two rational numbers
   """
   @spec subtract(a :: rational, b :: rational) :: rational
-  def subtract(a, b) do
+  def subtract({a1, a2}, {b1, b2}) do
+    reduce({a1 * b2 - a2 * b1, b1 * b2})
   end
 
   @doc """
   Multiply two rational numbers
   """
   @spec multiply(a :: rational, b :: rational) :: rational
-  def multiply(a, b) do
+  def multiply({a1, a2}, {b1, b2}) do
+    reduce({a1 * a2, b1 * b2})
   end
 
   @doc """
   Divide two rational numbers
   """
   @spec divide_by(num :: rational, den :: rational) :: rational
-  def divide_by(num, den) do
+  def divide_by({a1, a2}, {b1, b2}) do
+    reduce({a1 * b2, a2 * b1})
   end
 
   @doc """
   Absolute value of a rational number
   """
   @spec abs(a :: rational) :: rational
-  def abs(a) do
+  def abs({a1, a2}) do
+    reduce({Kernel.abs(a1), Kernel.abs(a2)})
   end
 
   @doc """
   Exponentiation of a rational number by an integer
   """
-  @spec pow_rational(a :: rational, n :: integer) :: rational
-  def pow_rational(a, n) do
+  def pow_rational({a, b}, n) when n <= 0 do
+    reduce({b ** Kernel.abs(n), a ** Kernel.abs(n)})
   end
+
+  def pow_rational({a, b}, n), do: reduce({a ** n, b ** n})
 
   @doc """
   Exponentiation of a real number by a rational number
   """
   @spec pow_real(x :: integer, n :: rational) :: float
-  def pow_real(x, n) do
+  def pow_real(x, {a, b}) do
+    x ** (a / b)
   end
 
   @doc """
   Reduce a rational number to its lowest terms
   """
-  @spec reduce(a :: rational) :: rational
-  def reduce(a) do
+  def reduce({a, b}) when b < 0 do
+    reduce({trunc(-a), trunc(-b)})
+  end
+
+  def reduce({a, b}) do
+    case Integer.gcd(trunc(a), trunc(b)) do
+      0 -> {trunc(a), trunc(b)}
+      gcd -> {trunc(a / gcd), trunc(b / gcd)}
+    end
   end
 end
