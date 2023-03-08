56fb68fb3ed52099f4f3bf50f3220affda804b6e
charlieroth <charlieroth4@icloud.com>
2023-03-08 16:31:56 +0100

    german-sysadmin: complete

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,5 +1,23 @@
 defmodule Username do
+  def sanitize(''), do: ''
+
   def sanitize(username) do
+    try do
+      username
+      |> to_string()
+      |> String.replace(~r/[\x{00DF}]/u, "ss")
+      |> String.replace(~r/[\x{00E4}]/u, "ae")
+      |> String.replace(~r/[\x{00F6}]/u, "oe")
+      |> String.replace(~r/[\x{00FC}]/u, "ue")
+      |> String.replace(~r/\d/u, "")
+      |> String.codepoints()
+      |> Enum.filter(&Regex.match?(~r/[\w_]/, &1))
+      |> Enum.join()
+      |> to_charlist()
+    rescue
+      _ -> ''
+    end
+
     # ä becomes ae
     # ö becomes oe
     # ü becomes ue
