2168c3f3df7a0c89c85d7895245c814c1be870dc
charlieroth <charlieroth4@icloud.com>
2023-03-09 15:02:04 +0100

    wine-cellar: complete

--- elixir/wine-cellar/lib/wine_cellar.ex
+++ elixir/wine-cellar/lib/wine_cellar.ex
@@ -1,15 +1,26 @@
 defmodule WineCellar do
   def explain_colors do
-    # Please implement the explain_colors/0 function
+    [
+      white: "Fermented without skin contact.",
+      red: "Fermented with skin contact using dark-colored grapes.",
+      rose: "Fermented with some skin contact, but not enough to qualify as a red wine."
+    ]
   end
 
   def filter(cellar, color, opts \\ []) do
-    # Please implement the filter/3 function
+    cellar
+    |> filter_by_color(color)
+    |> filter_by_year(opts[:year])
+    |> filter_by_country(opts[:country])
   end
+
+  defp filter_by_color(wines, color), do: Keyword.get_values(wines, color)
 
   # The functions below do not need to be modified.
 
   defp filter_by_year(wines, year)
+  # When opts[:year] is nil, return wines
+  defp filter_by_year(wines, nil), do: wines
   defp filter_by_year([], _year), do: []
 
   defp filter_by_year([{_, year, _} = wine | tail], year) do
@@ -21,6 +32,8 @@ defmodule WineCellar do
   end
 
   defp filter_by_country(wines, country)
+  # When opts[:country] is nil, return wines
+  defp filter_by_country(wines, nil), do: wines
   defp filter_by_country([], _country), do: []
 
   defp filter_by_country([{_, _, country} = wine | tail], country) do
