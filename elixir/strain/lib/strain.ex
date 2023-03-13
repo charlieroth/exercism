152509917e5be2e45fb0bb681a2e6860f951b7d1
charlieroth <charlieroth4@icloud.com>
2023-03-13 07:52:53 +0100

    strain: complete

--- elixir/strain/lib/strain.ex
+++ elixir/strain/lib/strain.ex
@@ -6,8 +6,21 @@ defmodule Strain do
   Do not use `Enum.filter`.
   """
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def keep([], _fun), do: []
+
   def keep(list, fun) do
+    keeper(list, [], fun)
   end
+
+  defp keeper([head | tail], acc, fun) do
+    if fun.(head) do
+      keeper(tail, [head | acc], fun)
+    else
+      keeper(tail, acc, fun)
+    end
+  end
+
+  defp keeper([], acc, _fun), do: Enum.reverse(acc)
 
   @doc """
   Given a `list` of items and a function `fun`, return the list of items where
@@ -16,6 +29,19 @@ defmodule Strain do
   Do not use `Enum.reject`.
   """
   @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
+  def discard([], _fun), do: []
+
   def discard(list, fun) do
+    discarder(list, [], fun)
   end
+
+  defp discarder([head | tail], acc, fun) do
+    if fun.(head) do
+      discarder(tail, acc, fun)
+    else
+      discarder(tail, [head | acc], fun)
+    end
+  end
+
+  defp discarder([], acc, _fun), do: Enum.reverse(acc)
 end
