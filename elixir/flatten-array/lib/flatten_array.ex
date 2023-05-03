023d02de1370e4f0ff43faf44cef624ef4d9c86b
charlieroth <charlieroth4@icloud.com>
2023-05-03 12:38:57 +0200

    elixir:flatten-array: wip

--- elixir/flatten-array/lib/flatten_array.ex
+++ elixir/flatten-array/lib/flatten_array.ex
@@ -14,5 +14,32 @@ defmodule FlattenArray do
 
   @spec flatten(list) :: list
   def flatten(list) do
+    no_nils = []
+
+    if list == [] do
+      no_nils
+    else
+      list
+      |> Enum.reduce(no_nils, fn el, acc ->
+        recur(el, acc)
+      end)
+    end
+  end
+
+  def recur(list, acc) when is_list(list) do
+    if length(list) == 0 do
+      acc
+    else
+      [hd | _rest] = list
+      recur(hd, acc)
+    end
+  end
+
+  def recur(el, acc) when is_integer(el) do
+    [el | acc]
+  end
+
+  def recur(el, acc) when is_nil(el) do
+    acc
   end
 end
